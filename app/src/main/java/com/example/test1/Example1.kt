0a7fbc887188fbb51d7b1cce4628d7ff5eff8d3f
bomblock <ekwotmd@gmail.com>
2023-06-19 23:01:19 +0900

    g

--- app/src/main/java/com/example/test1/Example1.kt
+++ app/src/main/java/com/example/test1/Example1.kt
@@ -1,20 +1,9 @@
 package com.example.test1
+// todo val var
 
 fun main() {
-    val result = test(1 )
-    test2(id = "하하", name = "히히", nickname = "헝헝")
-    println("Hello world!!")
-}
-
-//todo 2.함수
-
-
-fun test(a: Int, b: Int = 3) :Int {
-    val b=3
-    println(a+b)
-    return a+b
-}
-
-fun test2(name : String,nickname:String, id: String){
+    val user = User("이름",10)
 
 }
+
+class User(val nmae : String, var age: Int)
