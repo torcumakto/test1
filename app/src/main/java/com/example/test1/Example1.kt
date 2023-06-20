802a1126b4d7fe2ace76c1642a36f489d565df9a
bomblock <ekwotmd@gmail.com>
2023-06-20 20:53:54 +0900

    g

--- app/src/main/java/com/example/test1/Example1.kt
+++ app/src/main/java/com/example/test1/Example1.kt
@@ -3,7 +3,21 @@ package com.example.test1
 
 fun main() {
     val user = User("이름",10)
+    println(user.age)
+    val kid = Kid("아이", 3, "male")
 
 }
 
-class User(val nmae : String, var age: Int)
+open class User(open val name : String, open var age: Int=100)
+
+class Kid constructor(override val name:String,override var age :Int) : User(name, age) {
+    var gender :String = "female"
+
+    init {
+        println("초기화 중 입니다.")
+    }
+    constructor( name : String, age:Int,gender:String) : this (name,age){
+        this.gender = gender
+        println("부생성자 호출")
+    }
+}
