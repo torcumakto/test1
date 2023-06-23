8774e3eb316d558a75091620f8e22431705d60c3
bomblock <ekwotmd@gmail.com>
2023-06-23 21:16:32 +0900

    g

--- app/src/main/java/com/example/test1/Example1.kt
+++ app/src/main/java/com/example/test1/Example1.kt
@@ -3,7 +3,16 @@ package com.example.test1
 fun main() {
    val list = mutableListOf(1,2,3,4,5)
     list.add(6)
-    list.addAll
+    list.addAll(listOf(7,8,9))
+
+    val list1 = listOf(1,2,3,4)
+    list1[0]
+
+    list1.first { id = 10 }
+
+    val diverseList = listOf(1, "안녕", 1.78, true)
+    println(list.joinToString ("'"))
+
+    val map = mapOf((1 to "안녕"),(2 to "hello"))
 }
 
-torcumakto
